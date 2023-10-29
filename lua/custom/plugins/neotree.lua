b6c08baa23f52bc1570fc591574f356d671a4074
Justin M <zhengyi13@gmail.com>
2023-10-29 14:49:47 -0700

    add my first plugin: neotree config

---
+++ lua/custom/plugins/neotree.lua
@@ -0,0 +1,13 @@
+-- Configure NeoTree file tree plugin
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  version = "*",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    "MunifTanjim/nui.nvim",
+  },
+  config = function ()
+    require('neo-tree').setup {}
+  end,
+}
